09aa2b12b7c2a317eb700866ba4c458dc82d6ff8
Andy Gallagher <andy.gallagher@theguardian.com>
2020-09-15 16:25:53 +0100

    removing old code for gnm_masters and deprecated views

--- gnm_masters/exceptions.py
+++
@@ -1,5 +0,0 @@
-class NotAMasterError(Exception):
-    '''
-    The metadata from returned did not result in a valid Master object.
-    '''
-    pass
--- gnm_masters/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from portal.plugins.gnm_masters import urls  # noqa
-'''
-Must import everything from all classes containing tests.
-Probably because how plugins are loaded because standard nose-syntax to trigger specific ones does not work otherwise.
-'''
-from .models import *  # noqa
-from .views import *  # noqa
