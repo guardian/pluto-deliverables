7809f91dd2c4a51e13f8dcd7f0f5f53e20e9cb24
Dave Allison <david.allison@theguardian.com>
2022-03-16 13:16:54 +0000

    Get CDS route mapping from the settings.py file

--- rabbitmq/CDSResponderProcessor.py
+++ rabbitmq/CDSResponderProcessor.py
@@ -3,6 +3,7 @@ from gnm_deliverables.models import DeliverableAsset, Mainstream, DailyMotion
 import logging
 from .serializers import CDSMessageSerializer
 from .models import CDSResponderMessage
+from gnm_deliverables.settings import CDS_ROUTE_MAP
 
 logger = logging.getLogger(__name__)
 
@@ -23,18 +24,23 @@ def get_dailymotion_record(asset_id: int) -> DailyMotion:
         pass
 
 
+def get_route_mapping(routename: str) -> str:
+    return list(CDS_ROUTE_MAP.keys())[list(CDS_ROUTE_MAP.values()).index(routename)]
+
+
 class CDSResponderProcessor(MessageProcessor):
     routing_key = "cds.job.started"
     serializer = CDSMessageSerializer
 
     def valid_message_receive(self, exchange_name, routing_key, delivery_tag, body):
         msg = CDSResponderMessage(**body)
-        if msg.routename == 'MainstreamMedia.xml':
+        route_mapping = get_route_mapping(msg.routename)
+        if route_mapping == 'mainstream':
             mainstream = get_mainstream_record(int(msg.deliverable_asset))
             mainstream.routename = msg.routename
             mainstream.job_id = msg.job_name
             mainstream.save()
-        if msg.routename == 'DailyMotion.xml':
+        if route_mapping == 'dailymotion':
             dailymotion = get_dailymotion_record(int(msg.deliverable_asset))
             dailymotion.routename = msg.routename
             dailymotion.job_id = msg.job_name
@@ -47,11 +53,12 @@ class CDSInvalidProcessor(MessageProcessor):
 
     def valid_message_receive(self, exchange_name, routing_key, delivery_tag, body):
         msg = CDSResponderMessage(**body)
-        if msg.routename == 'MainstreamMedia.xml':
+        route_mapping = get_route_mapping(msg.routename)
+        if route_mapping == 'mainstream':
             mainstream = get_mainstream_record(int(msg.deliverable_asset))
             mainstream.upload_status = 'Upload Failed'
             mainstream.save()
-        if msg.routename == 'DailyMotion.xml':
+        if route_mapping == 'dailymotion':
             dailymotion = get_dailymotion_record(int(msg.deliverable_asset))
             dailymotion.upload_status = 'Upload Failed'
             dailymotion.save()
