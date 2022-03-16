4103e56390ad179e54e8a65f9774b907ee853c0c
Dave Allison <david.allison@theguardian.com>
2022-03-16 11:35:16 +0000

    Have two classes to deal with cds.job

--- rabbitmq/CDSResponderProcessor.py
+++ rabbitmq/CDSResponderProcessor.py
@@ -7,48 +7,53 @@ from .models import CDSResponderMessage
 logger = logging.getLogger(__name__)
 
 
+def get_mainstream_record(asset_id: int) -> Mainstream:
+    try:
+        asset = DeliverableAsset.objects.get(id=asset_id)
+        return asset.mainstream_master
+    except DeliverableAsset.DoesNotExist:
+        pass
+
+
+def get_dailymotion_record(asset_id: int) -> DailyMotion:
+    try:
+        asset = DeliverableAsset.objects.get(id=asset_id)
+        return asset.DailyMotion_master
+    except DeliverableAsset.DoesNotExist:
+        pass
+
+
 class CDSResponderProcessor(MessageProcessor):
-    routing_key = "cds.job.*"
+    routing_key = "cds.job.started"
     serializer = CDSMessageSerializer
 
-    def get_mainstream_record(self, asset_id: int) -> Mainstream:
-        try:
-            asset = DeliverableAsset.objects.get(id=asset_id)
-            return asset.mainstream_master
-        except DeliverableAsset.DoesNotExist:
-            pass
+    def valid_message_receive(self, exchange_name, routing_key, delivery_tag, body):
+        msg = CDSResponderMessage(**body)
+        if msg.routename == 'MainstreamMedia.xml':
+            mainstream = get_mainstream_record(int(msg.deliverable_asset))
+            mainstream.routename = msg.routename
+            mainstream.job_id = msg.job_name
+            mainstream.save()
+        if msg.routename == 'DailyMotion.xml':
+            dailymotion = get_dailymotion_record(int(msg.deliverable_asset))
+            dailymotion.routename = msg.routename
+            dailymotion.job_id = msg.job_name
+            dailymotion.save()
 
-    def get_dailymotion_record(self, asset_id: int) -> DailyMotion:
-        try:
-            asset = DeliverableAsset.objects.get(id=asset_id)
-            return asset.DailyMotion_master
-        except DeliverableAsset.DoesNotExist:
-            pass
+
+class CDSInvalidProcessor(MessageProcessor):
+    routing_key = "cds.job.invalid"
+    serializer = CDSMessageSerializer
 
     def valid_message_receive(self, exchange_name, routing_key, delivery_tag, body):
-        if (routing_key == 'cds.job.started') or (routing_key == 'cds.job.invalid'):
-            msg = CDSResponderMessage(**body)
-            logger.info(msg.__dict__)
-            if msg.routename == 'MainstreamMedia.xml':
-                logger.info(msg.deliverable_asset)
-                mainstream = self.get_mainstream_record(int(msg.deliverable_asset))
-                logger.info(mainstream.__dict__)
-                if routing_key == 'cds.job.started':
-                    mainstream.routename = msg.routename
-                    mainstream.job_id = msg.job_name
-                if routing_key == 'cds.job.invalid':
-                    mainstream.upload_status = 'Upload Failed'
-                mainstream.save()
-                logger.info(mainstream.__dict__)
-            if msg.routename == 'DailyMotion.xml':
-                dailymotion = self.get_dailymotion_record(int(msg.deliverable_asset))
-                if routing_key == 'cds.job.started':
-                    dailymotion.routename = msg.routename
-                    dailymotion.job_id = msg.job_name
-                if routing_key == 'cds.job.invalid':
-                    dailymotion.upload_status = 'Upload Failed'
-                dailymotion.save()
-
-
+        msg = CDSResponderMessage(**body)
+        if msg.routename == 'MainstreamMedia.xml':
+            mainstream = get_mainstream_record(int(msg.deliverable_asset))
+            mainstream.upload_status = 'Upload Failed'
+            mainstream.save()
+        if msg.routename == 'DailyMotion.xml':
+            dailymotion = get_dailymotion_record(int(msg.deliverable_asset))
+            dailymotion.upload_status = 'Upload Failed'
+            dailymotion.save()
 
 
--- rabbitmq/mappings.py
+++ rabbitmq/mappings.py
@@ -2,7 +2,7 @@ from .AtomResponderProcessor import AtomResponderProcessor
 from .AssetUpdatedProcessor import AssetUpdatedProcessor
 from .vidispine_message_processor import VidispineMessageProcessor
 from .StoragetierArchivedMessageProcessor import StoragetierArchivedMessageProcessor
-from .CDSResponderProcessor import CDSResponderProcessor
+from .CDSResponderProcessor import CDSResponderProcessor, CDSInvalidProcessor
 
 
 ##This structure is imported by name in the run_rabbitmq_responder
@@ -26,5 +26,9 @@ EXCHANGE_MAPPINGS = [
     {
         "exchange": "cdsresponder",
         "handler": CDSResponderProcessor(),
+    },
+    {
+        "exchange": "cdsresponder",
+        "handler": CDSInvalidProcessor(),
     }
 ]
