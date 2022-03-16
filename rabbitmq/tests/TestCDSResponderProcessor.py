bcbdb9235f79b12444afcab6eab627a8e53f09d5
Dave Allison <david.allison@theguardian.com>
2022-03-16 13:44:30 +0000

    New unit test

---
+++ rabbitmq/tests/TestCDSResponderProcessor.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+
+class TestCDSResponderProcessor(TestCase):
+    def test_get_route_mapping(self):
+        """
+        Makes sure the correct strings are output by get_route_mapping
+        :return:
+        """
+        from rabbitmq.CDSResponderProcessor import get_route_mapping
+
+        mapping = get_route_mapping('MainstreamMedia.xml')
+        self.assertEqual(mapping, 'mainstream')
+        mapping = get_route_mapping('DailyMotion.xml')
+        self.assertEqual(mapping, 'dailymotion')
+
+
+
+
+
+
